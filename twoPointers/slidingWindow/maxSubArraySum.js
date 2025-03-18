30e7180db6f37580d4269af05937ef30fccf8293
Gangadhar Mullapudi <gangadhar1373@gmail.com>
2025-03-17 22:39:49 -0400

    max subarry

--- twoPointers/slidingWindow/maxSubArraySum.js
+++ twoPointers/slidingWindow/maxSubArraySum.js
@@ -11,7 +11,7 @@ function maxSubArraySum1(arr, num) {
   return max;
 }
 
-function maxSubArraySum(arr, num) {
+function maxSubarraySum(arr, num) {
   if (arr.length < num) return null;
   let maxSum = 0;
   let tempSum = 0;
@@ -26,4 +26,9 @@ function maxSubArraySum(arr, num) {
   return maxSum;
 }
 
-console.log(maxSubArraySum([1, 12, -5, -6, 50, 3], 3));
+let a = maxSubarraySum([100, 200, 300, 400], 2); // 700
+let b = maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4); // 39
+let c = maxSubarraySum([-3, 4, 0, -2, 6, -1], 2); // 5
+let d = maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2); // 5
+let e = maxSubarraySum([2, 3], 3); // null
+console.log(a, b, c, d, e);
